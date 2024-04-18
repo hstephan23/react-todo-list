349612eb466b68a2703e514ccf38f6e9687932c9
hstephan23 <harrison@stephanclan.com>
2024-04-18 11:16:08 -0600

    added the necessary base pieces

---
+++ src/components/ListElement/index.jsx
@@ -0,0 +1,16 @@
+import ListItem from "../ListItem";
+
+function ListElement() {
+    
+    return (
+        <>
+            <h1>TODO LIST</h1>
+            <input type="text"></input>
+            <button>Submit</button>
+            <ListItem/>
+        </>
+    )
+};
+
+
+export default ListElement;
